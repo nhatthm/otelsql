ab44862af00ec71049b50677b32a59546559cd8f
nhatthm <thmnhat@gmail.com>
2022-05-17 20:49:01 +0200

    Add tidy workflow

---
+++ .github/scripts/pr_precondition.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+HEAD_REF=${GITHUB_HEAD_REF:-${GITHUB_REF_NAME}}
+BASE_REF="master"
+
+EXECUTE="true"
+
+make tidy
+
+if [[ $(git ls-files --modified | wc -l | xargs) -gt 0 ]]; then
+    EXECUTE="false"
+fi
+
+echo "::set-output name=passed::$EXECUTE"

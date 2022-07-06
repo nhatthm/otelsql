583f232743f40d02601036b6b4a0f390f7bdb2b5
Anthony Regeda <regeda@users.noreply.github.com>
2022-07-06 16:27:17 +0200

    Proper function name for milliseconds since. (#116)
    The function `microsecondsSince` returns milliseconds. Then the
    function's name has been changed to `millisecondsSince`.

--- time.go
+++ time.go
@@ -2,6 +2,6 @@ package otelsql
 
 import "time"
 
-func microsecondsSince(t time.Time) float64 {
-	return float64(time.Since(t).Nanoseconds()) / 1e6
+func millisecondsSince(t time.Time) float64 {
+	return float64(time.Since(t).Milliseconds())
 }
